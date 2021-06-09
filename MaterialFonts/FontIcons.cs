7b5947d1dc7897d94b808615c35dc2e74e4b1ae3
Jayson Ragasa <jayson.d.ragasa@live.com>
2021-06-09 22:53:36 +0800

    Update Wed 06/09/2021 / 22:53:35.39

---
+++ MaterialFonts/FontIcons.cs
@@ -0,0 +1,9 @@
+﻿namespace MaterialFonts.Fonts
+{
+    public static class IconFont
+	{
+		public const string Facebook = "\U000f020c";
+		public const string Instagram = "\U000f02fe";
+		public const string Jeepney = "\U000f0302";
+	}
+}
